8d61fb885924b7e69656c5a9e2a3191ed5c723f1
Dianablue321 <dianablue321@gmail.com>
2015-10-10 16:51:48 +0300

    show me what you got

--- hoveret2/Program.cs
+++ hoveret2/Program.cs
@@ -33,7 +33,7 @@ namespace hoveret2
         {
             double avg = 0;
             double markTest;
-            for (int i=0;i<3;i++)
+            for (int i = 0; i < 3; i++)
             {
                 Console.WriteLine(" Your test mark is:");
                 markTest = double.Parse(Console.ReadLine());
@@ -42,24 +42,79 @@ namespace hoveret2
                     markTest = markTest * 1.1;
                 }
 
-                avg =+ markTest;     
+                avg = +markTest;
             }
-            avg = avg / 3; 
+            avg = avg / 3;
             if (avg.Equals(100))
             {
                 Console.WriteLine(" Nice Job ya dork");
                 Console.ReadLine();
             }
             else
-            Console.WriteLine(" too bad stupid");
+                Console.WriteLine(" too bad stupid");
             Console.ReadLine();
 
+        }
+        static void Targil6()
+        {
+            int Bhavilot, Shavilot;
+            Console.WriteLine("what is the number of bought packages");
+            Bhavilot = int.Parse(Console.ReadLine());
+            Bhavilot = Bhavilot * 5;
+            Console.WriteLine("what is the number of sold packages");
+            Shavilot = int.Parse(Console.ReadLine());
+            Shavilot = Shavilot * 10;
+            if (Bhavilot >= Shavilot)
+            {
+                Console.WriteLine("you are going broke");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("you are getting more money");
+                Console.ReadLine();
+            }
+
+
+        }
+        static void Targil7()
+        {
+            List<int> palindrom = new List<int>() ;
+            int number;
+            bool truth_flag = true;
+            Console.WriteLine("Show me what you got");
+            number = int.Parse(Console.ReadLine());
+            while(number>0)
+            {
+                palindrom.Add(number % 10);
+                number = (number - (number % 10)) / 10;
+            }
+            int list_length = palindrom.Count;
+            for (int i=0; i< list_length; i++)
+            {
+                if (!palindrom[i].Equals(palindrom[(list_length -1) -i ]))
+                {
+                    truth_flag = false;
+                }
+            }
+
+            if (truth_flag)
+            {
+                Console.WriteLine("I like what you got");
+            }
+            else
+            {
+                Console.WriteLine("Prepare to be destroyed");
+            }
+            Console.ReadLine();
         }
 
         static void Main(string[] args)
         {
             //Program.Targil2();
-            Program.Targil3a();
+            //rogram.Targill3a();
+            //Program.Targil6();
+            Program.Targil7();
         }
     }
 }
